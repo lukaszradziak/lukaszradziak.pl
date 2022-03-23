3fe3e6cc338fb07a4f2758aac30870b550ee2a7d
Łukasz Radziak <lukasilestam@gmail.com>
2022-03-23 01:23:21 +0100

    Add title in <head>

--- components/layout.js
+++ components/layout.js
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import useLocalStorage from "hooks/useLocalStorage";
 
 import Footer from "./footer";
@@ -7,16 +8,23 @@ export default function Layout({ children, data }) {
   const [darkMode, setDarkMode] = useLocalStorage(`theme`, `light`);
 
   return (
-    <div className={darkMode === `dark` ? `dark` : ``}>
-      <div className={`min-h-screen bg-gray-50 dark:bg-gray-800`}>
-        <Header
-          setting={data.setting}
-          darkMode={darkMode}
-          setDarkMode={setDarkMode}
-        />
-        <main>{children}</main>
-        <Footer setting={data.setting} />
+    <>
+      <Head>
+        <title>{data.setting.title}</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta property="og:title" content={data.setting.title} key="title" />
+      </Head>
+      <div className={darkMode === `dark` ? `dark` : ``}>
+        <div className={`min-h-screen bg-gray-50 dark:bg-gray-800`}>
+          <Header
+            setting={data.setting}
+            darkMode={darkMode}
+            setDarkMode={setDarkMode}
+          />
+          <main>{children}</main>
+          <Footer setting={data.setting} />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
