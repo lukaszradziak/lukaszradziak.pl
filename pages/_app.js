40343b162badc2863c156d1c55cd81c3b17fa302
Łukasz Radziak <lukasz.radziak@icloud.com>
2022-08-05 15:49:08 +0200

    Add stats script

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
+import Script from 'next/script'
 import "@/styles/index.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Script async defer data-website-id="82aa1129-0f3a-41d8-810d-dbbc5fe88c55" src="https://umami-sandy-three.vercel.app/u.js" />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
