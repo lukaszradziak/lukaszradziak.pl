206b99b51366d85f7ceb50246b9533063d968345
Łukasz Radziak <lukasz.radziak@icloud.com>
2023-04-08 14:27:23 +0200

    Remove stats.

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,8 @@
-import Script from 'next/script'
 import "@/styles/index.css";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Script async defer data-website-id="f49d748c-b363-4492-a846-595d3fa447d5" src="https://stats.lukaszradziak.pl/u.js" />
       <Component {...pageProps} />
     </>
   );
