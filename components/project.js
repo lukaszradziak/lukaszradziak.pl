b0440d56b8df3cd21528c766660f8e34e4ece277
Łukasz Radziak <lukasz.radziak@icloud.com>
2023-11-06 23:47:45 +0100

    Project hover effect + settings for IntelliJ IDE

--- components/project.js
+++ components/project.js
@@ -3,17 +3,24 @@ import icons from "@/lib/icons";
 
 export default function Project({ data }) {
   return (
-    <div className="flex flex-col rounded-lg shadow-lg overflow-hidden">
+    <div className="flex flex-col rounded-lg shadow-lg overflow-hidden hover:opacity-80">
       <div className="flex-shrink-0">
         {data.thumb ? (
-          <Image
-            className="h-48 w-full object-cover"
-            width={600}
-            height={400}
-            priority={true}
-            src={data.thumb.url}
-            alt={data.title}
-          />
+          <a
+            href={data.url}
+            target="_blank"
+            rel="noreferrer"
+            className="block mt-2"
+          >
+            <Image
+              className="h-48 w-full object-cover"
+              width={600}
+              height={400}
+              priority={true}
+              src={data.thumb.url}
+              alt={data.title}
+            />
+          </a>
         ) : null}
       </div>
       <div className="flex-1 bg-white dark:bg-gray-800 p-6 flex flex-col justify-between">
