66e3cb467d9deb8074b0a3b3d5f14ff303b6f081
Łukasz Radziak <lukasz.radziak@icloud.com>
2026-06-05 01:18:59 +0200

    Replace CSS with Tailwind CSS, update dependencies, and configure Tailwind plugin for Vite

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,20 @@ import Contact from './pages/Contact'
 
 function App() {
   return (
-    <>
-      <nav>
+    <div className="w-3xl mx-auto">
+      <nav className="flex gap-4 bg-gray-200 p-4">
         <Link to="/">Home</Link>
         <Link to="/projects">Projects</Link>
         <Link to="/contact">Contact</Link>
       </nav>
+      <div>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/projects" element={<Projects />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
-    </>
+      </div>
+    </div>
   )
 }
 
